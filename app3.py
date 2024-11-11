bd0a93e623b417d47682e5f15874b3bbf652af69
ponkis01 <152533644+ponkis01@users.noreply.github.com>
2024-11-11 21:22:51 +0100

    debugging

--- app3.py
+++ app3.py
@@ -5,32 +5,16 @@ def main():
 
     
     st.title("Spotify Melody Match")
-    st.write("Willkommen bei - Melody Match! Wähle zwei Playlists aus.")
+    st.write("Willkommen bei - Melody Match! Finde die Playlist für dich und deine Freunde")
 
     # Musikpräferenzen
-    st.header("Wähle 2 deiner Lieblingsplaylists in Spotify")
+    st.header("Finde deinen Match!")
 
     # Auswahlmöglichkeiten für Genres
     playlists = ['Pop', 'Rock', "Music", 'Hip-Hop', 'Jazz', 'Classical']
     selected_playlists = st.multiselect("Wähle deine Playlists:", playlists)
 
-    # Auswahlmöglichkeiten für Künstler
-    artists = ['Taylor Swift', 'The Weeknd', 'Ed Sheeran', 'Beyoncé', 'Adele']
-    selected_artists = st.multiselect("Wähle deine Lieblingskünstler:", artists)
-
-    # Button für den Mix mit Machine Learning
-    if st.button("Mix Up"):
-        # Hier kommt die Logik für das Zusammenmischen der Songs
-        st.write("Mixing up your preferences...")  # Placeholder für Machine Learning Logik
-
-        # Beispielausgabe, die du später anpassen kannst
-        st.write("Empfohlene Songs für dich könnten sein: ...")
-
-    st.slider("tempo", min_value = 0.0, max_value=1.0, value = 0.5, step = 0.1)
-    Lieblingssong = st.text_input("Dein Song:")
-
-
-    # Liste der Künstler
+     # Liste der Künstler
     artists = ["Taylor Swift", "Drake", "Beyoncé", "Adele", "The Weeknd", "Billie Eilish", "Ed Sheeran"]
 
     # Variable für ausgewählte Künstler
@@ -50,6 +34,17 @@ def main():
          break
 
     st.write("Ausgewählte Künstler:", selected_artists)
+
+    # Button für den Mix mit Machine Learning
+    if st.button("Mix Up"):
+        # Hier kommt die Logik für das Zusammenmischen der Songs
+        st.write("Mixing up your preferences...")  # Placeholder für Machine Learning Logik
+
+        # Beispielausgabe, die du später anpassen kannst
+        st.write("Empfohlene Songs für dich könnten sein: ...")
+
+    st.slider("tempo", min_value = 0.0, max_value=1.0, value = 0.5, step = 0.1)
+    Lieblingssong = st.text_input("Dein Song:")
 
 if __name__ == "__main__":
     main()
