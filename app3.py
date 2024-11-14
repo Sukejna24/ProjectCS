c1860aafceddcff0d68917d76d02b8d756b2eaaf
ponkis01 <152533644+ponkis01@users.noreply.github.com>
2024-11-14 19:45:51 +0100

    update

--- app3.py
+++ app3.py
@@ -1,33 +1,6 @@
 import streamlit as st
 import time
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
-# Spotify API-Anmeldeinformationen
-CLIENT_ID = '503970174ce54084923698fe550336f4'
-CLIENT_SECRET = '5b5397398bf84dd2a8e06b808c280e71'
-REDIRECT_URI = 'https://projectcs-kfdnyesodcfqp5dnwrihwp.streamlit.app/'
-
-# Funktion zur Authentifizierung und Erstellung eines Spotify-Objekts
-def authenticate_spotify():
-    scope = 'user-library-read user-top-read playlist-modify-public'
-    sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
-    
-    auth_url = sp_oauth.get_authorize_url()
-    st.write(f"Bitte [klicke hier, um dich anzumelden]({auth_url}) und kopiere den Code in das folgende Feld.")
-
-    # Nutzer gibt den Code ein
-    code = st.text_input("Füge den Authentifizierungs-Code hier ein:")
-
-    if code:
-        try:
-            token_info = sp_oauth.get_access_token(code)
-            sp = spotipy.Spotify(auth=token_info['access_token'])
-            return sp  # Gibt das authentifizierte Spotify-Objekt zurück
-        except Exception as e:
-            st.error(f"Authentifizierungsfehler: {e}")
-            return None
-    return None
 
 def main():
 
---
+++ import spotipy.py
@@ -0,0 +1,28 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Spotify API-Anmeldeinformationen
+CLIENT_ID = 'dein_spotify_client_id'
+CLIENT_SECRET = 'dein_spotify_client_secret'
+REDIRECT_URI = 'http://localhost:8888/callback'
+
+# Funktion zur Authentifizierung und Erstellung eines Spotify-Objekts
+def authenticate_spotify():
+    scope = 'user-library-read user-top-read playlist-modify-public'
+    sp_oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
+    
+    auth_url = sp_oauth.get_authorize_url()
+    st.write(f"Bitte [klicke hier, um dich anzumelden]({auth_url}) und kopiere den Code in das folgende Feld.")
+
+    # Nutzer gibt den Code ein
+    code = st.text_input("Füge den Authentifizierungs-Code hier ein:")
+
+    if code:
+        try:
+            token_info = sp_oauth.get_access_token(code)
+            sp = spotipy.Spotify(auth=token_info['access_token'])
+            return sp  # Gibt das authentifizierte Spotify-Objekt zurück
+        except Exception as e:
+            st.error(f"Authentifizierungsfehler: {e}")
+            return None
+    return None
