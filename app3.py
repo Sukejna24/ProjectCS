6b73691aadeae7b0a00a0df1432adba334173d42
ilir-r1 <ilir.ramadani@student.unisg.ch>
2024-11-14 14:47:08 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -52,17 +52,6 @@ def main():
     if selected_artists:
         st.write(f"You selected: {', '.join(selected_artists)}")
         
- # Zeige die Playlists nebeneinander
-        col1, col2 = st.columns(2)
-        
-     with col1:
-            st.subheader(selected_playlists[0])  # Titel der ersten Playlist
-            st.write("Details or tracks of the playlist go here...")  # Placeholder-Inhalt
-
-        with col2:
-            st.subheader(selected_playlists[1])  # Titel der zweiten Playlist
-            st.write("Details or tracks of the playlist go here...")  # Placeholder-Inhal
-
     # Button für den Mix mit Machine Learning
     if st.button("Mix Up"):
         # Hier kommt die Logik für das Zusammenmischen der Songs
