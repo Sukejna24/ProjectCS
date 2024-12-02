5d70e08c6963c8037cd663748177e6e71becc942
ilir-r1 <ilir.ramadani@student.unisg.ch>
2024-12-02 19:26:32 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -9,6 +9,7 @@ def main():
     .stButton > button {background-color: #1db954; color: white; font size: 16px; padding: 10px; border-radius: 5px; border: none;}
     .stButton > button:hover { background color: #1ed760;}
     .stSlider .st-bb {color: #1db954}
+    .stMultiselect select { background-color: #1ed760; color: white; border: none; border-radius: 5px; padding: 8px;}
     </style> """, unsafe_allow_html=True)
     
     # Seitenleiste mit Text und anderen Elementen
